ab231453a8b3af641570246ccca305e873d2e13c
Erik C. Thauvin <erik@thauvin.net>
2021-03-18 23:33:48 -0700

    Updated copryright.

--- src/main/java/net/thauvin/erik/httpstatus/taglibs/CauseTag.java
+++ src/main/java/net/thauvin/erik/httpstatus/taglibs/CauseTag.java
@@ -1,7 +1,7 @@
 /*
  * CauseTag.java
  *
- * Copyright (c) 2015-2020, Erik C. Thauvin (erik@thauvin.net)
+ * Copyright (c) 2015-2021, Erik C. Thauvin (erik@thauvin.net)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

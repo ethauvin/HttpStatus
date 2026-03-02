bcbbb6467cfec6c8a503fa7e5ef19e42a8966f4c
Erik C. Thauvin <erik@thauvin.net>
2026-03-02 15:58:38 -0800

    Use IOTools to create test results directory

--- src/bld/java/net/thauvin/erik/httpstatus/HttpStatusBuild.java
+++ src/bld/java/net/thauvin/erik/httpstatus/HttpStatusBuild.java
@@ -38,7 +38,7 @@ import rife.bld.extension.JUnitReporterOperation;
 import rife.bld.extension.JacocoReportOperation;
 import rife.bld.extension.PmdOperation;
 import rife.bld.extension.SpotBugsOperation;
-import rife.bld.extension.tools.IOUtils;
+import rife.bld.extension.tools.IOTools;
 import rife.bld.publish.*;
 import rife.tools.exceptions.FileUtilsErrorException;
 
@@ -57,7 +57,7 @@ public class HttpStatusBuild extends Project {
             .fromProject(this)
             .failOnViolation(true)
             .ruleSets("config/pmd.xml");
-    final File testResultsDirectory = IOUtils.resolveFile(buildDirectory(), "test-results", "test");
+    final File testResultsDirectory = IOTools.resolveFile(buildDirectory(), "test-results", "test");
 
     public HttpStatusBuild() {
         pkg = "net.thauvin.erik.httpstatus";
